eb1f0e87a6ab80578c17eabbb67a7513895e4730
ThaiKha <williamalbus26@gmail.com>
2024-02-02 00:50:38 +0700

    Test Eslint

---
+++ Index.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-redeclare
+    const name = 'James
+    const person = {first: name};
+console.log(person.first) // Outputs: { first: "James" }
+const sayHelloLinting = () =>
+{
+	console.log(`Hello linting, ${person.first} Bond`)
+}
+
+
+
+// Code lỗi do eslint
